e2f386dc963ce5d26b9bcdd80dfe2f67090865df
Toshihiro Kamiya <kamiya@mbj.nifty.com>
2017-07-16 01:49:17 +0900

    add option -s

--- json2jqq/__init__.py
+++ json2jqq/__init__.py
@@ -6,12 +6,13 @@ import ijson
 from .version import __version__
 
 
-def extract_queries_from_json(inp, internal_nodes=False):
+def extract_queries_from_json(inp, internal_nodes=False, sample_values=False):
     if isinstance(inp, str):
         inp = io.StringIO(inp)
 
     prefix_set = set()
     array_prefix_set = set()
+    sample_value_table = {}
 
     for prefix, event, value in ijson.parse(inp):
         if event == 'start_array':
@@ -20,33 +21,44 @@ def extract_queries_from_json(inp, internal_nodes=False):
             pass
         else:
             # 'boolean', 'number', 'string', or 'null'
+            if sample_values and (value is not None or event == 'null') and prefix not in prefix_set:
+                sample_value_table[prefix] = value
             prefix_set.add(prefix)
 
     array_prefixes = list(array_prefix_set)
     array_prefixes.sort(key=len, reverse=True)
 
     queries = []
+    svt = {}
     for p in sorted(prefix_set):
+        q = p
         for ap in array_prefixes:
-            if p == ap or p.startswith(ap) and p[len(ap)] == '.':
-                p = ap[:-4] + '[]' + p[len(ap):]
-        p = '.' + p.replace('.[]', '[]')
-        queries.append(p)
+            if q == ap or q.startswith(ap) and q[len(ap)] == '.':
+                q = ap[:-4] + '[]' + q[len(ap):]
+        q = '.' + q.replace('.[]', '[]')
+        queries.append(q)
+        if sample_values and p in sample_value_table:
+            svt[q] = sample_value_table[p]
 
-    return queries
+    if sample_values:
+        return queries, svt
+    else:
+        return queries
 
 
 __doc__ = """Extract query templates for jq tool from json data.
 
-Usage: json2jqq [-a] < data.json
+Usage: json2jqq [options] < data.json
 
 Options:
   -a    Show internal (non-leaf) nodes.
+  -s    Show samples (the first values for keys). 
 """
 
 
 def main():
     option_all_nodes = False
+    option_samples = False
     for a in sys.argv[1:]:
         if a in ('-h', '--help'):
             print(__doc__)
@@ -54,14 +66,31 @@ def main():
         elif a in ('-v', '--version'):
             print("json2qq %s" % __version__)
             return
-        elif a == '-a':
-            option_all_nodes = True
+        elif a.startswith('-'):
+            for op in a[1:]:
+                if op == 'a':
+                    option_all_nodes = True
+                elif op == 's':
+                    option_samples = True
+                else:
+                    sys.exit("error: unknown option: %s" % repr(op))
         else:
             sys.exit("error: too many command-line argument.")
 
-    qs = extract_queries_from_json(sys.stdin, internal_nodes=option_all_nodes)
-    print('\n'.join(qs))
-
+    r = extract_queries_from_json(sys.stdin, internal_nodes=option_all_nodes,
+            sample_values=option_samples)
+    if option_samples:
+        queries, sample_value_dict = r
+        for q in queries:
+            if q in sample_value_dict:
+                v = sample_value_dict[q]
+                vstr = 'null' if v is None else str(v)
+                print('%s\t%s' % (q, vstr))
+            else:
+                print('%s\t' % q)
+    else:
+        queries = r
+        print('\n'.join(queries))
 
 
 if __name__ == '__main__':
--- test/test_json2jqq.py
+++ test/test_json2jqq.py
@@ -18,7 +18,7 @@ class TestStringMethods(unittest.TestCase):
         queries = extract_queries_from_json(json_data, internal_nodes=True)
         self.assertEqual(queries, ['.', '.first', '.second'])
 
-    def test_single_map_of_map(self):
+    def test_map_of_map(self):
         json_data = '''{
     "first": {"first-first": 11, "first-second": 12}, 
     "second": {"second-first": 21, "second-second": 22}
@@ -41,12 +41,40 @@ class TestStringMethods(unittest.TestCase):
         queries = extract_queries_from_json(json_data, internal_nodes=True)
         self.assertEqual(queries, ['.', '.[]'])
 
-    def test_single_array_of_array(self):
+    def test_array_of_array(self):
         json_data = '[[1, 2, 3],[10,20,30]]'
         queries = extract_queries_from_json(json_data)
         self.assertEqual(queries, ['.[][]'])
         queries = extract_queries_from_json(json_data, internal_nodes=True)
         self.assertEqual(queries, ['.', '.[]', '.[][]'])
+
+    def test_null_value(self):
+        json_data = '''{
+    "first": {"first-first": 11, "first-second": null}, 
+    "second": {"second-first": 21, "second-second": 22}
+}'''
+        queries = extract_queries_from_json(json_data)
+        self.assertEqual(queries, ['.first.first-first', '.first.first-second', '.second.second-first', '.second.second-second'])
+        queries = extract_queries_from_json(json_data, internal_nodes=True)
+        self.assertEqual(queries, [
+            '.',
+                '.first',
+                    '.first.first-first', '.first.first-second',
+                '.second',
+                    '.second.second-first', '.second.second-second'
+        ])
+        q, d = extract_queries_from_json(json_data, sample_values=True)
+        self.assertEqual(q, ['.first.first-first', '.first.first-second', '.second.second-first', '.second.second-second'])
+        self.assertEqual(d, {'.first.first-first': 11, '.first.first-second': None, '.second.second-first':21, '.second.second-second':22 })
+        q, d = extract_queries_from_json(json_data, internal_nodes=True, sample_values=True)
+        self.assertEqual(q, [
+            '.',
+                '.first',
+                    '.first.first-first', '.first.first-second',
+                '.second',
+                    '.second.second-first', '.second.second-second'
+        ])
+        self.assertEqual(d, {'.first.first-first': 11, '.first.first-second': None, '.second.second-first':21, '.second.second-second':22 })
 
 
 if __name__ == '__main__':
